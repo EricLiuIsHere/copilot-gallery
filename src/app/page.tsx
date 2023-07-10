3e717e5f323a0f5b3b2e5a510ef7a3eb868d66f8
tian1ll1 <357637200@qq.com>
2023-07-11 01:56:50 +0800

    rollback previous change is the links changed to 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         </video>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-5 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://github.com/features/copilot/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
