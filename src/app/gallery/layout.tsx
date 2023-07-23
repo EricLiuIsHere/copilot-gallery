4d755ddbc5621ea66d5378213e52c6a8e339af34
Eric.Liu <marvelous_doer@hotmail.com>
2023-07-23 23:18:35 +0800

    add single play video using dynamic router

--- src/app/gallery/layout.tsx
+++ src/app/gallery/layout.tsx
@@ -8,21 +8,30 @@ export default function GalleryLayout({
   children: React.ReactNode
 }) {
   const pathName = usePathname();
-  const pathSegments = pathName.split('/').filter((segment) => segment !== '').map((segment) => {
+  const pathSegments = pathName.split('/').filter((segment) => segment !== '').map((segment, index, array) => {
+    if (index === array.length - 1 && !isNaN(Number(segment))) {
+      return null;
+    }
     return segment.charAt(0).toUpperCase() + segment.slice(1);
-  });
+  }).filter((segment) => segment !== null);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <nav className="text-gray-500 text-sm self-start">
         <Link href="/">
           <span className="hover:text-gray-700">Home</span>
         </Link>
-        {pathSegments.length > 0 && (
+        {pathSegments.map((segment, index) => (
           <>
             <span className="mx-2">{'>'}</span>
-            <span className="font-medium">{pathSegments[pathSegments.length - 1]}</span>
+            {index === pathSegments.length - 1 ? (
+              <span className="font-medium">{segment}</span>
+            ) : (
+              <Link href={`/${pathSegments.slice(0, index + 1).join('/').toLowerCase()}`}>
+                {segment}
+              </Link>
+            )}
           </>
-        )}
+        ))}
       </nav>
 
       {children}
